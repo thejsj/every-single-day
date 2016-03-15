4f6edf0ce74af0dcd7b3e459fe300735e8792c18
thejsj <jorge.silva@thejsj.com>
2016-03-15 01:26:13 -0700

    Change data management to Redux

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,9 @@
 import React, { AppRegistry, Component, StyleSheet, NavigatorIOS } from 'react-native'
 
-const MainListView = require('./lib/components/main/main-list-view.js')
-const AddNewHabitView = require('./lib/components/habit/add-new-habit-view.js')
-const HabitStorage = require('./lib/storage/habit-storage.js')
-const habitStorage = new HabitStorage()
+import store from './lib/stores'
+import MainListView from './lib/components/main/main-list-view.js'
+import AddNewHabitView from './lib/components/habit/add-new-habit-view.js'
+import { Provider } from 'react-redux'
 
 const styles = StyleSheet.create({
   navigator: {
@@ -13,35 +13,29 @@ const styles = StyleSheet.create({
 
 class EverySingleDay extends Component {
 
-  constructor () {
-    super()
-    habitStorage.init()
-      .then(this.forceUpdate.bind(this))
-    habitStorage
-      .onUpdate(this.forceUpdate.bind(this))
-  }
-
   render () {
     return (
-      <NavigatorIOS
-        style={styles.navigator}
-        ref='nav'
-        initialRoute={{
-          component: MainListView,
-          title: 'Every Single Day',
-          passProps: { habitStorage: habitStorage },
-          rightButtonTitle: 'Add',
-          onRightButtonPress: () => {
-            this.refs.nav.push({
-              component: AddNewHabitView,
-              title: 'Add New',
-              leftButtonTitle: 'Back',
-              passProps: { habitStorage: habitStorage },
-              onLeftButtonPress: () => this.refs.nav.pop()
-            })
-          }
-        }}
-      />
+      <Provider store={store}>
+        <NavigatorIOS
+          style={styles.navigator}
+          ref='nav'
+          initialRoute={{
+            component: MainListView,
+            title: 'Every Single Day',
+            passProps: { store: store },
+            rightButtonTitle: 'Add',
+            onRightButtonPress: () => {
+              this.refs.nav.push({
+                component: AddNewHabitView,
+                title: 'Add New',
+                leftButtonTitle: 'Back',
+                passProps: { store: store },
+                onLeftButtonPress: () => this.refs.nav.pop()
+              })
+            }
+          }}
+        />
+      </Provider>
     )
   }
 
